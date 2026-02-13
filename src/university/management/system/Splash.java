ec016d81469c81a984ae8a967a949e7c46d589af
krn-zorel <kumar.karanq98@gmail.com>
2026-02-13 22:39:58 +0530

    Created package folders

---
+++ src/university/management/system/Splash.java
@@ -0,0 +1,17 @@
+package university.management.system;
+
+import javax.swing.*;
+
+public class Splash extends JFrame{
+
+    Splash(){
+        setSize(500, 300);
+        setVisible(true);
+    }
+    public static void main(String[] args){
+        // Splash s = new Splash();
+        new Splash();
+
+
+    }
+}
