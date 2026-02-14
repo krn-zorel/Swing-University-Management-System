70bf3ce62c9ebb314a3400c0d6f15d0a1919dcc8
krn-zorel <kumar.karanq98@gmail.com>
2026-02-14 23:16:08 +0530

    Created JFrame Window

--- src/university/management/system/Splash.java
+++ src/university/management/system/Splash.java
@@ -5,8 +5,11 @@ import javax.swing.*;
 public class Splash extends JFrame{
 
     Splash(){
-        setSize(500, 300);
+        setLocation(150,80);
+        setSize(1000, 700);
         setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
     }
     public static void main(String[] args){
         // Splash s = new Splash();
