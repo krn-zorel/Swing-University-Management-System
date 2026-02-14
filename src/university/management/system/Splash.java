ff130b7f34bde6877635857076cca5584dc1c54c
krn-zorel <kumar.karanq98@gmail.com>
2026-02-14 23:25:48 +0530

    Created Jframe with image

--- src/university/management/system/Splash.java
+++ src/university/management/system/Splash.java
@@ -1,10 +1,20 @@
 package university.management.system;
 
+import java.awt.Image;
+
 import javax.swing.*;
+import java.awt.*;
 
 public class Splash extends JFrame{
 
     Splash(){
+
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/first.jpg"));
+        Image i2 = i1.getImage().getScaledInstance(1000,700, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel image = new JLabel(i3);
+        add(image);
+
         setLocation(150,80);
         setSize(1000, 700);
         setVisible(true);
