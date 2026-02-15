ac488d1aa0c76f7ef6fe49cb7df38ad312761a26
krn-zorel <kumar.karanq98@gmail.com>
2026-02-15 23:50:39 +0530

    Added Labels and style for starting

--- src/university/management/system/Login.java
+++ src/university/management/system/Login.java
@@ -6,19 +6,48 @@ import java.awt.*;
 public class Login extends JFrame{
     
     Login(){
-
-        getContentPane().setBackground(Color.GRAY);
-
-        JLabel labelForUsername = new JLabel("Username");
-        add(labelForUsername);
+        //Window Color and Setting the layout to null
+        getContentPane().setBackground(Color.BLACK);
+        setLayout(null);
         
+        //Window
         setSize(600,300);
-        setLocation(500,250);
-        setVisible(true);
+        // setLocation(500,250); //Obsolete code
+        setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Username Label
+        JLabel labelForUsername = new JLabel("Username");
+        labelForUsername.setBounds(50,50,100,20);
+        labelForUsername.setBackground(Color.WHITE);
+        add(labelForUsername);
+
+        //Username Input
+        JTextField inputForUsername = new JTextField();
+        inputForUsername.setBounds(130,50,100,20);
+        add(inputForUsername);
+
+
+        //Password Label
+        JLabel labelForPassword = new JLabel("Password");
+        labelForPassword.setBounds(50,100,100,20);
+        labelForPassword.setBackground(Color.WHITE);
+        add(labelForPassword);
+
+        //Password Input
+        JPasswordField inputForPassword = new JPasswordField();
+        inputForPassword.setBounds(130,100,100,20);
+        add(inputForPassword);
+
+        //Best Practice to render this at last
+        setVisible(true);
 
     }
     public static void main(String[] args){
-        new Login();
+        // new Login(); //Obsolete
+        //Modern and Safe
+        SwingUtilities.invokeLater(() -> {
+            new Login();
+        });
     }
 }
